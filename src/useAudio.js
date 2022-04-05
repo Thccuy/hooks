b38fb308a0ca045a5b65c873d0993897f66579eb
Thc <65441251+Thccuy@users.noreply.github.com>
2022-04-05 18:06:03 +0800

    Create useAudio.js

---
+++ src/useAudio.js
@@ -0,0 +1,65 @@
+import {useEffect, useRef, useState} from "react";
+import useInitial from "../../utils/hooks/useInitial";
+
+
+function useAudio(
+    src,
+
+    onNoSrc = () => {},
+) {
+    const [currentTime,setCurrentTime] = useState(0)
+    const [duration,setDuration] = useState(0)
+    // const [duration,setDuration] = useState(0)
+
+    const audio = useRef(null)
+    const onTimeUpdate = () => {
+        setCurrentTime(audio.current.currentTime)
+    }
+    const changeCurrentTime = (time) => {
+        audio.current.currentTime = time
+    }
+    const changeCurrentTimeRef = useRef(changeCurrentTime)
+    const onTimeUpdateRef = useRef(onTimeUpdate)
+
+
+    useInitial(() => {
+        audio.current = new Audio(src)
+        audio.current.addEventListener("timeupdate",onTimeUpdateRef.current)
+    })
+
+    const play = () => {
+        audio.current.play()
+            .then(() => {
+                setDuration(audio.current.duration)
+            })
+            .catch((err) => {
+                if(err === "DOMException: The element has no supported sources.") {
+                    onNoSrc()
+                }
+            })
+    }
+
+    const pause = () => {
+        audio.current.pause()
+    }
+
+    //Clean up event
+    useEffect(() => () => {
+        audio.current.removeEventListener("timeupdate",onTimeUpdateRef.current)
+    },[])
+
+    // load music when src Change
+    useEffect(() => {
+        audio.current.src = src
+    },[src])
+
+    return {
+        duration,
+        currentTime,
+        play,
+        pause,
+        changeCurrentTime:changeCurrentTimeRef.current
+    }
+}
+
+export default useAudio
